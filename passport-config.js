d5c94d5b4f5a802e106c8567ae0e2c5e1c803072
tinytrebuchet <tinytrebuchet@protonmail.com>
2022-02-26 12:46:40 +0530

    implement login system with mongodb, and added basic routes on home page

--- passport-config.js
+++ passport-config.js
@@ -3,8 +3,8 @@ const bcrypt = require('bcrypt')
 
 function initialize(passport, getUserByEmail, getUserById) {
   const authenticateUser = async (email, password, done) => {
-    const user = getUserByEmail(email)
-    if (user == null) {
+    const user = await getUserByEmail(email)
+    if (user == undefined) {
       return done(null, false, { message: 'No user with that email' })
     }
 
@@ -21,9 +21,7 @@ function initialize(passport, getUserByEmail, getUserById) {
 
   passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
   passport.serializeUser((user, done) => done(null, user.id))
-  passport.deserializeUser((id, done) => {
-    return done(null, getUserById(id))
-  })
+  passport.deserializeUser( async (id, done) => done(null, await getUserById(id)))
 }
 
 module.exports = initialize
--- server.js
+++ server.js
@@ -9,15 +9,17 @@ const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb://localhost/hack36", { family: 4 })
 
 const initializePassport = require('./passport-config')
+const User = require('./User')
 initializePassport(
   passport,
-  email => users.find(user => user.email === email),
-  id => users.find(user => user.id === id)
+  (email) => User.findOne({ "email": { $eq: email } }),
+  (id) => User.findOne({ "_id": { $eq: id } })
 )
-
-const users = []
 
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
@@ -52,8 +54,7 @@ app.get('/register', checkNotAuthenticated, (req, res) => {
 app.post('/register', checkNotAuthenticated, async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    users.push({
-      id: Date.now().toString(),
+    await User.create({
       name: req.body.name,
       email: req.body.email,
       password: hashedPassword
@@ -68,6 +69,21 @@ app.delete('/logout', (req, res) => {
   req.logOut()
   res.redirect('/login')
 })
+
+
+
+app.get('/timetable', checkAuthenticated, (req, res) => {
+  res.render('timetable.ejs')
+})
+
+app.get('/courses', checkAuthenticated, (req, res) => {
+  res.render('courses.ejs')
+})
+
+app.get('/attendance', checkAuthenticated, (req, res) => {
+  res.render('attendance.ejs')
+})
+
 
 function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
